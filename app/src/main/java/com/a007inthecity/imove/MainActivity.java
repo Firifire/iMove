127e6a7d74299d0c65d10a2f40c1198958018558
Firifire <firifire@live.com>
2018-12-06 14:45:50 +0800

    Added Location class Added modifiers for global variable usage

--- app/src/main/java/com/a007inthecity/imove/MainActivity.java
+++ app/src/main/java/com/a007inthecity/imove/MainActivity.java
@@ -1,13 +1,28 @@
 package com.a007inthecity.imove;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -61,6 +76,23 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        LocationManager locationManager = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
+
+        LocationListener locationListener = new locationListener();
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            try {
+                locationManager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
+            } catch (Exception e) {
+                //TODO: confirm if exception can occur
+            }
+        }
+        else{
+            //TODO: permission is not granted
+        }
+
     }
 
     private void setFragment(Fragment fragment) {
@@ -69,5 +101,48 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.commit();
 
     }
+
+    private class locationListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(Location loc) {
+            Toast.makeText(
+                    getBaseContext(),
+                    "Location changed: Lat: " + loc.getLatitude() + " Lng: "
+                            + loc.getLongitude(), Toast.LENGTH_SHORT).show();
+            String longitude = "Longitude: " + loc.getLongitude();
+            Log.v("TAG", longitude);
+            String latitude = "Latitude: " + loc.getLatitude();
+            Log.v("TAG", latitude);
+
+            /*------- To get city name from coordinates -------- */
+            String cityName = null;
+            Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
+            List<Address> addresses;
+            try {
+                addresses = gcd.getFromLocation(loc.getLatitude(),
+                        loc.getLongitude(), 1);
+                if (addresses.size() > 0) {
+                    System.out.println(addresses.get(0).getLocality());
+                    cityName = addresses.get(0).getLocality();
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            String s = longitude + "\n" + latitude + "\n\nMy Current City is: "
+                    + cityName;
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {}
+
+        @Override
+        public void onProviderEnabled(String provider) {}
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {}
+    }
+
 
 }
---
+++ app/src/main/java/com/a007inthecity/imove/Modifiers.java
@@ -0,0 +1,27 @@
+package com.a007inthecity.imove;
+
+public class Modifiers {
+
+    enum method {bus, metro, walking, cycling, carPooling, driving};
+
+    public class publicTransport{
+
+    }
+
+    public class walking{
+
+    }
+
+    public class cycling{
+
+    }
+
+
+    public class carPooling{
+
+    }
+
+    public class driving{
+
+    }
+}
